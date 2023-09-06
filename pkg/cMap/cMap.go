cad41ae8aa06a3f1e7f8a814440bba651af3b56e
meowt <matweewilya@gmail.com>
2023-09-06 20:04:02 +0300

    customMap pkg release

--- pkg/cMap/cMap.go
+++ pkg/cMap/cMap.go
@@ -1,6 +1,11 @@
 package cMap
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"os"
 	"sync"
 	"time"
 )
@@ -12,20 +17,20 @@ type CMap struct {
 }
 
 type Value struct {
-	v         interface{}
-	expiresAt time.Time
+	V         interface{} `json:"value"`
+	ExpiresAt time.Time   `json:"expires_at"`
 }
 
-func New() *CMap {
+func New(d time.Duration) *CMap {
 	cm := &CMap{
 		m:  map[string]Value{},
 		mu: sync.Mutex{},
 	}
 	go func() {
-		for now := range time.Tick(time.Second) {
+		for now := range time.Tick(d) {
 			cm.mu.Lock()
 			for i, v := range cm.m {
-				if !v.expiresAt.IsZero() && v.expiresAt.After(now) {
+				if !v.ExpiresAt.IsZero() && now.After(v.ExpiresAt) {
 					delete(cm.m, i)
 				}
 			}
@@ -37,10 +42,10 @@ func New() *CMap {
 
 func (cm *CMap) Put(key string, v interface{}, ttl time.Duration) {
 	value := Value{
-		v: v,
+		V: v,
 	}
 	if ttl != 0 {
-		value.expiresAt = time.Now().Add(ttl)
+		value.ExpiresAt = time.Now().Add(ttl)
 	}
 
 	cm.mu.Lock()
@@ -51,5 +56,71 @@ func (cm *CMap) Put(key string, v interface{}, ttl time.Duration) {
 func (cm *CMap) Get(key string) (value interface{}) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
-	return cm.m[key]
+	return cm.m[key].V
+}
+
+func (cm *CMap) StoreToFile(f *os.File, interval time.Duration) (err error) {
+	var mapData []byte
+	go func() {
+		for range time.Tick(interval) {
+			cm.mu.Lock()
+			if mapData, err = json.Marshal(cm.m); err != nil {
+				log.Println(err)
+				return
+			}
+			cm.mu.Unlock()
+
+			//Check of file's openness
+			_, err = f.Read([]byte{})
+			if err != nil && err != io.EOF {
+				break
+			}
+
+			//Truncate old data
+			if err = f.Truncate(0); err != nil {
+				log.Println(err)
+				return
+			}
+
+			//Move file's cursor to 0 byte
+			if _, err = f.Seek(0, 0); err != nil {
+				log.Println(err)
+				return
+			}
+
+			//Write map's data to file
+			if _, err = f.Write(mapData); err != nil {
+				log.Println(err)
+				return
+			}
+
+			//Guarantee that data is on disk
+			if err = f.Sync(); err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}()
+	return
+}
+
+func (cm *CMap) LoadFromFile(f *os.File) (*CMap, error) {
+	jsonData, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	//Empty file check
+	if string(jsonData) == "" {
+		return nil, errors.New("empty load-file")
+	}
+
+	//Unmarshalling json into map
+	cm.mu.Lock()
+	if err = json.Unmarshal(jsonData, &cm.m); err != nil {
+		return nil, err
+	}
+	cm.mu.Unlock()
+
+	return cm, nil
 }
--- pkg/cMap/cMap_test.go
+++ pkg/cMap/cMap_test.go
@@ -1,6 +1,9 @@
 package cMap
 
 import (
+	"os"
+	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
@@ -27,7 +30,7 @@ func TestCMap_Put(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cm := New()
+			cm := New(100 * time.Millisecond)
 			for i := 0; i < 1000; i++ {
 				go func() {
 					cm.Put(tt.args.key, tt.args.v, tt.args.ttl)
@@ -37,6 +40,127 @@ func TestCMap_Put(t *testing.T) {
 					t.Logf("got: %v\n", cm.Get(tt.args.key))
 				}()
 			}
+		})
+	}
+}
+
+func TestCMap_Get(t *testing.T) {
+	type args struct {
+		key   string
+		value interface{}
+		ttl   time.Duration
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantValue   interface{}
+		timeToSleep time.Duration
+	}{
+		// TODO: Add test cases.
+		{
+			args:      args{key: "1", value: "one"},
+			wantValue: "one",
+		},
+		{
+			name:        "expired value",
+			args:        args{key: "1", value: "one", ttl: 200 * time.Millisecond},
+			wantValue:   nil,
+			timeToSleep: time.Second,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cm := New(100 * time.Millisecond)
+			cm.Put(tt.args.key, tt.args.value, tt.args.ttl)
+			time.Sleep(tt.timeToSleep)
+			if gotValue := cm.Get(tt.args.key); !reflect.DeepEqual(gotValue, tt.wantValue) {
+				t.Errorf("Get() = %v, want %v", gotValue, tt.wantValue)
+			}
+		})
+	}
+}
+
+func TestCMap_StoreToFile(t *testing.T) {
+	type args struct {
+		interval time.Duration
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{args: args{interval: time.Second}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//Creating new map
+			cm := New(100 * time.Millisecond)
+
+			//Inserting some data
+			cm.Put("1", "one", 0)
+			cm.Put("2", "two", 2*time.Second)
+			cm.Put("3", "three", 0)
+			cm.Put("4", "four", 0)
+			cm.Put("5", "five", 10*time.Second)
+
+			//Opening a storage file
+			f, err := os.OpenFile("cMapStorage.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Write() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			defer f.Close()
+
+			//Executing function
+			err = cm.StoreToFile(f, 100*time.Millisecond)
+
+			//Checking the result
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Write() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			time.Sleep(2 * time.Second)
+			//Random exiting case
+			//os.Exit(10)
+		})
+	}
+}
+
+func TestCMap_LoadFromFile(t *testing.T) {
+	type fields struct {
+		m  map[string]Value
+		mu sync.Mutex
+	}
+	type args struct {
+		f *os.File
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *CMap
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//Opening a storage file
+			f, err := os.OpenFile("cMapStorage.json", os.O_RDWR, 0755)
+			defer f.Close()
+
+			//Executing function
+			cm, err := New(100 * time.Millisecond).LoadFromFile(f)
+
+			//Checking results
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LoadFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			t.Logf("%v", cm)
 		})
 	}
 }
